68e668211c338969b89b1f6e5a405206d3d3bc04
br.uno <brunomengaldo@gmail.com>
2019-12-20 16:34:06 -0300

    Acabou semestre 2019.2

--- crush.cpp
+++ crush.cpp
@@ -1,63 +1,83 @@
 #include <iostream>
-#include <string>
+#include <list>
+#include <queue>
+#define INFINITO 10000000
 
 using namespace std;
 
+class Grafo {
+private:
+	int V; // número de vértices
+	list<pair<int, int> > * adj; // ponteiro para um array contendo as listas de adjacências
+
+public:
+	// construtor
+	Grafo(int V){
+		this->V = V;
+		adj = new list<pair<int, int> >[V];
+	}
+
+	void addAresta(int v1, int v2, int custo){
+		adj[v1].push_back(make_pair(v2, custo));
+	}
+
+	// algoritmo de Dijkstra
+	int dijkstra(int orig, int dest){
+		int dist[V];
+		int visitados[V];
+
+		priority_queue < pair<int, int>,
+					   vector<pair<int, int> >, greater<pair<int, int> > > pq;
+
+		for(int i = 0; i < V; i++){
+			dist[i] = INFINITO;
+			visitados[i] = false;
+		}
+
+		dist[orig] = 0;
+
+		pq.push(make_pair(dist[orig], orig));
+
+		while(!pq.empty()){
+			pair<int, int> p = pq.top(); // extrai o pair do topo
+			int u = p.second; // obtém o vértice do pair
+			pq.pop(); // remove da fila
+
+			if(visitados[u] == false){
+				visitados[u] = true;
+				list<pair<int, int> >::iterator it;
+				for(it = adj[u].begin(); it != adj[u].end(); it++){
+					int v = it->first;
+					int custo_aresta = it->second;
+					if(dist[v] > (dist[u] + custo_aresta)){
+						dist[v] = dist[u] + custo_aresta;
+						pq.push(make_pair(dist[v], v));
+					}
+				}
+			}
+		}
+		return dist[dest];
+	}
+};
 
 
-//func pra montar a matriz de adjacencia
-int** matriz_adj(int v, int e, int** in){
 
-    // alocando
-    int **matriz = (int**) malloc(v * sizeof(int*));
-    for(int i=0; i<v; i++){
-        matriz[i] = (int *) malloc(v * sizeof(int));
-    }
-
-
-    // zerante
-    for(int i=0; i<v; i++)
-        for(int j=0; j<v; j++)
-            matriz[i][j]=0;
-
-    // arrumante
-    for(int i=0; i<e; i++){
-        int partindo = in[i][0];
-        int recebendo = in[i][1];
-        int peso = in[i][2];
-        if(partindo!=recebendo){ //sem autoloop
-            matriz[partindo][recebendo]=peso;
-        }
-    }
-
-    // retornar a braba
-    return matriz;
-}
-
-int caminho(int **matriz){
-    //
-    return 0;
-}
 
 
 int main(int argc, char const *argv[]){
 
-    char file_name[100];
-    
+    char file_name[100]; 
     int v, e;
 
     // arquivo
     FILE *entrada;
-
     scanf("%s", file_name);
     entrada = fopen(file_name, "r");
     if (entrada == NULL){
-        printf("\nNão encontrei o arquivo!\n");
+        printf("\nNao encontrei o arquivo!\n");
         exit(EXIT_FAILURE);
     }
-
     fscanf(entrada, "%d %d ", &v, &e);
-    //getc(entrada);
 
     //receber os inputs
     int **arestas = (int**) malloc(sizeof(int*) *e );
@@ -67,19 +87,16 @@ int main(int argc, char const *argv[]){
     }
         
 
-    //matriz adjacentte criada
-    int **adj=matriz_adj(v, e, arestas);
 
-//*  impressão da matriz de adjacencia    
-    for(int i=0; i<v; i++){
-        printf("linha de %2d: ", i);
-        for(int j=0; j<v; j++)
-            printf("%2d ", adj[i][j]);
-        printf("\n");
+
+    Grafo dale(v);
+    for(int i=0; i<e; i++){
+        dale.addAresta(arestas[i][0], arestas[i][1], arestas[i][2]);
     }
-//*/
+    cout << dale.dijkstra(0, v-1) << endl;
 
-    //manipulação to do..
+
+
 
 
     //desaloc
